a545215b4347035a382e7c70b93a2772fc6bbe91
Sung Bae <singsang2@gmail.com>
2018-09-28 04:14:57 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -3,11 +3,31 @@
 $(document).ready(function(){
 
 // call functions here
+getIt();
 
 });
 
 function getIt(){
-  $('p').addEventListener("click", function(){
-    alert("Hey!")
-  })
+  $('p').on("click", function(){
+    alert("Hey!");
+  });
+}
+
+function frameIt(){
+  
+}
+
+function pressIt(){
+  $('input').on("keydown",function(e){
+    if(e.key === "G"){
+      alert("G was pressed!");
+    }
+    
+  });
+}
+
+function submitIt(){
+  $('submit').on("submit", function(){
+    alert("Your form is going to be submitted now.");
+  });
 }
