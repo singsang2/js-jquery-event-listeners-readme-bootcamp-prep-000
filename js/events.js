3c97d6e910e552f9f9d8e036e2b090b61a9ba9d7
Sung Bae <singsang2@gmail.com>
2018-09-28 04:43:17 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -3,5 +3,36 @@
 $(document).ready(function(){
 
 // call functions here
+getIt();
+frameIt();
+pressIt();
+submitIt();
 
 });
+
+function getIt(){
+  $('p').on("click", function(){
+    alert("Hey!");
+  });
+}
+
+function frameIt(){
+  $('img').on("load", function(){
+    $('img').addClass("tasty");
+  });
+}
+
+function pressIt(){
+  $('input').on("keydown",function(e){
+    if(e.which === 71){
+      alert("G was pressed!");
+    }
+    
+  });
+}
+
+function submitIt(){
+  $('form').on("submit", function(){
+    alert("Your form is going to be submitted now.");
+  });
+}
